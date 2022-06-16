ab5baa40a2cc86c4311e8093d2cae848effe11a4
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2022-06-16 14:36:12 +0100

    install:add webpack and webpack-cli packages and add build script to pck.json

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+(() => {
+  "use strict";
+  console.log(function () {
+    return "What do you call a fake noodle? An Impasta!";
+  });
+})();
---
+++ src/generateJoke.js
@@ -0,0 +1,4 @@
+function generateJoke() {
+  return "What do you call a fake noodle? An Impasta!";
+}
+export default generateJoke;
