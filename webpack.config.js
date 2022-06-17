35d2bd7e837b975420972391199691822b97cb5a
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2022-06-17 19:51:35 +0100

    feat:add babel loader for backward compatibility of older browsers

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
+    clean: true,
   },
+  devtool: "source-map",
   devServer: {
     static: {
       directory: path.resolve(__dirname, "dist"),
@@ -23,6 +25,16 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
